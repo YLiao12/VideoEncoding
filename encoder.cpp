5b8e92d5449f41c7493942feba1a11f09e9643db
YLiao12 <seu_lyq@outlook.com>
2022-05-19 21:44:37 +0800

    re-organize the code and network trace data

--- encoder.cpp
+++ encoder.cpp
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <algorithm>
 #include <fstream>
+#include <vector>
 #include "stdint.h"
-
-//opencv
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/opencv.hpp>
 
 using namespace std;
 extern "C"
@@ -19,40 +16,29 @@ int main(int argc, char **argv)
 {
 
      int ret;
+     int ret_re_encode;
      int y_size;
      int i, j;
-     // using opencv get device camara
-     cv::VideoCapture cap;
-     if (!cap.open(0))
-     {
-          printf("Video capture device failure!\n");
-          return 0;
-     }
-     int cv_width = 1280, cv_height = 720;
-     cap.set(cv::CAP_PROP_FRAME_WIDTH, cv_width);
-     cap.set(cv::CAP_PROP_FRAME_HEIGHT, cv_height);
+     
+     FILE *fp_src = fopen("concert_1080.yuv", "rb");
 
-     //FILE* fp_src  = fopen("../cuc_ieschool_640x360_yuv444p.yuv", "rb");
-     FILE *fp_src = fopen("test1.yuv", "rb");
-
-     FILE *fp_dst = fopen("test1_2M_80_0ms.h264", "wb");
-     FILE *fp_output_dst = fopen("test1_2M_output_80_0ms.txt", "wt+");
-     FILE *fp_delta_dst = fopen("test1_2M_delta_80_0ms.txt", "wt+");
-     // FILE *fp_bps_dst = fopen("Johnny_bps_720_4M.txt", "wt+");
-     // FILE *fp_bps_delta_dst = fopen("Johnny_bps_delta_720_4M.txt", "wt+");
-     // FILE *fp_time = fopen("Jonny_donothing.txt", "wt+");
-     //Encode 50 frame
-     //if set 0, encode all frame
+     FILE *fp_dst = fopen("concert_1080_algo_5G.h264", "wb");
+     FILE *fp_output_dst = fopen("concert_1080_output_algo_5G.txt", "wt+");
+     FILE *fp_delta_dst = fopen("concert_1080_delta_algo_5G.txt", "wt+");
+     FILE *fp_l_delta_dst = fopen("concert_1080_Ldelta_algo_5G.txt", "wt+");
+     
      ifstream infile;
-     infile.open("bandwidthbps.txt");
-     int bandwidth[654] = {0};
-     for (int i = 0; i < 654; i++)
+     infile.open("5G_5min.txt");
+     int bandwidth[7823] = {0};
+     for (int i = 0; i < 7823; i++)
      {
           infile >> bandwidth[i];
+          bandwidth[i] /= 5;
      }
      int frame_num = 0;
      int csp = X264_CSP_I420;
-     int width = 1280, height = 720;
+     // 1920 1080; 1280 720
+     int width = 1920, height = 1080;
 
      int iNal = 0;
      x264_nal_t *pNals = NULL;
@@ -97,7 +83,7 @@ int main(int argc, char **argv)
      pParam->rc.i_vbv_buffer_size = 1600;
      // pParam->rc.i_lookahead = 50;
      pParam->i_bframe = 0;
-     // pParam->i_fps_num = 25;
+     pParam->i_fps_num = 25;
      pParam->b_intra_refresh = 1;
      pParam->i_frame_reference = 1;
 
@@ -109,10 +95,11 @@ int main(int argc, char **argv)
      x264_picture_alloc(pPic_in, csp, pParam->i_width, pParam->i_height);
 
      //ret = x264_encoder_headers(pHandle, &pNals, &iNal);
-     unsigned int luma_size = width * height;;
+     unsigned int luma_size = width * height;
+     ;
      unsigned int chroma_size = luma_size / 4;
-     int bufLen = width*height*3/2;
-     uint8_t* pYuvBuf = new uint8_t[bufLen];
+     int bufLen = width * height * 3 / 2;
+     uint8_t *pYuvBuf = new uint8_t[bufLen];
      y_size = pParam->i_width * pParam->i_height;
      //detect frame number
      if (frame_num == 0)
@@ -141,46 +128,33 @@ int main(int argc, char **argv)
      //Loop to Encode
      int frame_number = 0;
 
-     float decrease_flag = 1.1925;
+     float change_param = 1.0;
+     float larger_param = 1.3;
+     int larger_flag = 1;
+     float biggest_delay = 0.0;
+     float ave_delay = 0.0;
+     float sum_delay = 0.0;
+     int budget = 0;
+     int delayed_frames = 0;
+     float delay_ratio;
+     int over2frames = 0;
+     int over4frames = 0;
+     float over2ratio;
+     float over4ratio;
+
+     int target = bandwidth[0];
+     float time = 2.0;
+     float buffer = 3.0;
+
+     vector<float> ratioList;
+     vector<float> deltaList;
+
+     int biggerCount = 0;
+     int constrainCount = 0;
+     float previousN = 2;
 
      for (i = 0; i < frame_num; i++)
      {
-          // cv::Mat frame;
-          // cap >> frame;
-          // if (frame.empty())
-          //      break; // end of video stream
-          // cv::imshow("Sender", frame);
-          // if (cv::waitKey(10) == 27)
-          // {
-          //      return 0;
-          // }
-          // cv::Mat yuv;
-          // cv::cvtColor(frame, yuv, 128);
-          // memcpy(pYuvBuf, yuv.data, bufLen * sizeof(unsigned char));
-          // int frame_pointer = 0;
-          // uint8_t *plane1 = pPic_in->img.plane[0];
-          // for (int i = 0; i < luma_size; i++)
-          // {
-          //      *plane1 = pYuvBuf[frame_pointer];
-          //      plane1++;
-          //      frame_pointer++;
-          // }
-          // uint8_t *plane2 = pPic_in->img.plane[1];
-          // for (int i = 0; i < chroma_size; i++)
-          // {
-          //      *plane2 = pYuvBuf[frame_pointer];
-          //      plane2++;
-          //      frame_pointer++;
-          // }
-          // uint8_t *plane3 = pPic_in->img.plane[2];
-          // for (int i = 0; i < chroma_size; i++)
-          // {
-          //      *plane3 = pYuvBuf[frame_pointer];
-          //      plane3++;
-          //      frame_pointer++;
-          // }
-          // pPic_in->i_pts = frame_number;
-
           switch (csp)
           {
           case X264_CSP_I444:
@@ -205,29 +179,61 @@ int main(int argc, char **argv)
           }
           pPic_in->i_pts = i;
 
-          pParam->rc.i_bitrate = bandwidth[i / 25] / (2 * decrease_flag); //  unit: kbit/s
-          pParam->rc.i_vbv_max_bitrate = bandwidth[i / 25] / (2 * decrease_flag);
-          pParam->rc.i_vbv_buffer_size = bandwidth[i / 25];
+          // i_bitrate = bandwidth[i]  -> Base
+          pParam->rc.i_bitrate = target; //  unit: kbit/s
+          pParam->rc.i_vbv_max_bitrate = target;
+          pParam->rc.i_vbv_buffer_size = target;
           pParam->b_intra_refresh = 1;
           pParam->i_frame_reference = 1;
 
           int reconfig = x264_encoder_reconfig(pHandle, pParam);
           ret = x264_encoder_encode(pHandle, &pNals, &iNal, pPic_in, pPic_out);
+          
           if (ret < 0)
           {
                printf("Error.\n");
                return -1;
           }
 
-          // if (ret >= pParam->rc.i_bitrate * 5) {
-          //      // cout << pParam->rc.i_bitrate;
-          //      decrease_flag = 1.3;
-          // }
-          // else
-          //      decrease_flag = 1.0;
+          if (ret - (bandwidth[i] * previousN) * 5 > 0)
+               biggerCount++;
+          // if (ret - target * 5 > 0)
+          //      biggerCount++;
 
+          float previous80ratio = 0;
+
+          previousN = (delay_time + 0.12 - consuming_time ) / 0.04;
+
+          /**
+           * Statistical Encoder
+           * 
+           */
+          target = bandwidth[i + 1] * ((delay_time + 0.12 - consuming_time ) / 0.04) / 1.712865;
+          if (target > bandwidth[i + 1]) {
+               target = bandwidth[i + 1];
+               constrainCount++;
+          }
+          if (target < 0)
+               target = 100;
+
+          /**
+           * Match Encoder
+           * 
+           */
+          // budget = ret - bandwidth[i] * 5;
+          // // (bandwidth[i / 25] / 2) bandwidth_size
+          // // pParam->rc.i_bitrate * 5 target_size
+          // // to make 90% sure next frame will compensate the budget / 1.19254
+          // target = (bandwidth[(i + 1)] - budget / 5) / 1.19254;
+          // if (target < 0)
+          //      target = 100;
+
+          /**
+           * @brief delay calculation
+           * 
+           */
           // do nothing if the frame arriving time has to be delayed
-          float consuming_time_i = (float)(ret * 8) / (float)(bandwidth[i / 25] * 500);
+          float consuming_time_i = (float)(ret * 8) / (float)(bandwidth[i] * 1000);
 
           consuming_time += consuming_time_i;
           delay_time += 0.04;
@@ -250,6 +256,17 @@ int main(int argc, char **argv)
           // fprintf(fp_time, "frame: %d  consuming_time: %.4f   delay_time %.4f  delay_flag:%d \n",
           //                          i, consuming_time, delay_time, delay_frame_flag);
 
+          delayed_frames += delay_frame_flag;
+          if (consuming_time - delay_time > 0.08)
+          {
+               over2frames++;
+          }
+
+          if (consuming_time - delay_time > 0.16)
+          {
+               over4frames++;
+          }
+
           //delay until next frame or drop the frame
           int drop_frame_flag = 0;
           if (consuming_time_delay > delay_time_delay)
@@ -258,26 +275,30 @@ int main(int argc, char **argv)
                consuming_time_delay = delay_time_delay;
           }
 
-          // bitratebps += ret;
-          // if (i % 25 == 0)
-          // {
-          //      fprintf(fp_bps_dst, "frame: %d  target bps: %d   actual bps: %d\n", i, pParam->rc.i_bitrate, bitratebps / 125);
-          //      fprintf(fp_bps_delta_dst, "%d\n", pParam->rc.i_bitrate - bitratebps / 125);
-          //      bitratebps = 0;
-          // }
 
-          printf("Succeed encode frame: %5d %d %.4f\n",i, ret, decrease_flag);
+          if (consuming_time - delay_time > biggest_delay)
+               biggest_delay = consuming_time - delay_time;
+          sum_delay += consuming_time - delay_time;
+          ave_delay = sum_delay / (i + 1);
+
+          printf("Succeed encode frame: %5d %d %d %d %.4f\n", i, ret, bandwidth[i] * 5, target * 5, consuming_time - delay_time);
           fprintf(fp_output_dst, "frame: %d  bandwidth:%d  target size: %d   actual size: %d consuming_time: %.4f   delay_time %.4f  buffer_time: %.4f delay_flag: %d drop_flag: %d\n",
-                  i, (bandwidth[i / 25] / 2) * 5, pParam->rc.i_bitrate * 5, ret, consuming_time, delay_time, consuming_time - delay_time, delay_frame_flag, drop_frame_flag);
-          float delta = (float)((bandwidth[i / 25] / 2) * 5 - ret) * 100 / (float)((bandwidth[i / 25] / 2) * 5);
+                  i, bandwidth[i] * 5, pParam->rc.i_bitrate * 5, ret, consuming_time, delay_time, consuming_time - delay_time, delay_frame_flag, drop_frame_flag);
+          float delta = (float)(ret - (bandwidth[i]) * 5) / (float)((bandwidth[i]) * 5);
+          float ldelta = (float)((((0.08 - consuming_time + delay_time) / 0.04) * bandwidth[i]) * 5 - ret) * 100 / (float)((((0.08 - consuming_time + delay_time) / 0.04) * bandwidth[i]) * 5);
           fprintf(fp_delta_dst, "%.4f\n", delta);
+          fprintf(fp_l_delta_dst, "%.4f\n", ldelta);
 
           for (j = 0; j < iNal; ++j)
           {
                //if (drop_frame_flag != 1)
-                    fwrite(pNals[j].p_payload, 1, pNals[j].i_payload, fp_dst);
+               fwrite(pNals[j].p_payload, 1, pNals[j].i_payload, fp_dst);
           }
+          float ratio = (float) ret / (float) (target * 5);
+          ratioList.push_back(ratio);
+          deltaList.push_back(delta);
      }
+     
      i = 0;
      //flush encoder
      while (1)
@@ -296,6 +317,31 @@ int main(int argc, char **argv)
      }
      x264_picture_clean(pPic_in);
      x264_encoder_close(pHandle);
+
+     delay_ratio = (float)delayed_frames / (float)frame_num;
+     over2ratio = (float)over2frames / (float)frame_num;
+     over4ratio = (float)over4frames / (float)frame_num;
+
+     /**
+      * @brief Calculating the CDF propability, the value will only be useful 
+      * when the target size for every frame is bwe
+      * 
+      */
+     sort(deltaList.begin(), deltaList.end());
+     
+     float ratio95 = deltaList[deltaList.size() * 0.95];
+     float ratio90 = deltaList[deltaList.size() * 0.9];
+     float ratio80 = deltaList[deltaList.size() * 0.8];
+     cout << "biggest_delay: " << biggest_delay << "s, ave_delay: " << ave_delay << "s. " << endl;
+     cout << "delay frames: " << delayed_frames << ", delay ratio: " << delay_ratio << endl;
+     cout << "over 2-frame buffer frames: " << over2frames << ", over 2-frame buffer ratio: " << over2ratio << endl;
+     cout << "over 4-frame buffer frames: " << over4frames << ", over 4-frame buffer ratio: " << over4ratio << endl;
+     cout << "95% CDF: " << ratio95 << endl;
+     cout << "90% CDF: " << ratio90 << endl;
+     cout << "80% CDF: " << ratio80 << endl;
+     cout << "bigger frames: " << biggerCount << endl;
+     cout << "constraint frames: " << constrainCount << endl;
+
      pHandle = NULL;
 
      free(pPic_in);
